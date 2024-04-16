458bfdf959e24cd237826ab86ba5067ff428034b
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-16 16:23:39 +0500

    user id success done

--- controller/product/uploadProduct.js
+++ controller/product/uploadProduct.js
@@ -1,31 +1,40 @@
-const uploadProductPermission = require("../../helpers/permission")
-const productModel = require("../../models/productModel")
+const uploadProductPermission = require("../../helpers/permission");
+const productModel = require("../../models/productModel");
 
-async function UploadProductController(req,res){
-    try{
-        const sessionUserId = req.userId
+async function UploadProductController(req, res) {
+    try {
+        const sessionUserId = req.userId;
 
-        if(!uploadProductPermission(sessionUserId)){
-            throw new Error("Permission denied")
+        if (!uploadProductPermission(sessionUserId)) {
+            throw new Error("Permission denied");
         }
-    
-        const uploadProduct = new productModel(req.body)
-        const saveProduct = await uploadProduct.save()
+        
+        // Extract user ID from the request
+        const userId = req.body.userId || sessionUserId;
+
+        // Create the product object with user's ID
+        const productData = {
+            ...req.body,
+            userId: userId
+        };
+
+        const uploadProduct = new productModel(productData);
+        const saveProduct = await uploadProduct.save();
 
         res.status(201).json({
-            message : "Product upload successfully",
-            error : false,
-            success : true,
-            data : saveProduct
-        })
+            message: "Product uploaded successfully",
+            error: false,
+            success: true,
+            data: saveProduct
+        });
 
-    }catch(err){
+    } catch (err) {
         res.status(400).json({
-            message : err.message || err,
-            error : true,
-            success : false
-        })
+            message: err.message || err,
+            error: true,
+            success: false
+        });
     }
 }
 
-module.exports = UploadProductController
+module.exports = UploadProductController;
