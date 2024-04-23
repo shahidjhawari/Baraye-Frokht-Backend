c6b918aa7add618916b67313f7b504354628f482
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-23 07:34:03 +0500

    latest all done

--- controller/user/userSignIn.js
+++ controller/user/userSignIn.js
@@ -1,93 +1,65 @@
-import React, { useContext, useEffect, useState } from "react";
-import fetchCategoryWiseProduct from "../helpers/fetchCategoryWiseProduct";
-import displayINRCurrency from "../helpers/displayCurrency";
-import { Link } from "react-router-dom";
-import addToCart from "../helpers/addToCart";
-import Context from "../context";
-import scrollTop from "../helpers/scrollTop";
+const bcrypt = require('bcryptjs')
+const userModel = require('../../models/userModel')
+const jwt = require('jsonwebtoken');
 
-const CategroyWiseProductDisplay = ({ category, heading }) => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const loadingList = new Array(13).fill(null);
+async function userSignInController(req,res){
+    try{
+        const { email , password} = req.body
 
-  const { fetchUserAddToCart } = useContext(Context);
+        if(!email){
+            throw new Error("Please provide email")
+        }
+        if(!password){
+             throw new Error("Please provide password")
+        }
 
-  const handleAddToCart = async (e, id) => {
-    await addToCart(e, id);
-    fetchUserAddToCart();
-  };
+        const user = await userModel.findOne({email})
 
-  const fetchData = async () => {
-    setLoading(true);
-    const categoryProduct = await fetchCategoryWiseProduct(category);
-    setLoading(false);
+       if(!user){
+            throw new Error("User not found")
+       }
 
-    console.log("horizontal data", categoryProduct.data);
-    setData(categoryProduct?.data);
-  };
+       const checkPassword = await bcrypt.compare(password,user.password)
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+       console.log("checkPassoword",checkPassword)
 
-  return (
-    <div className="container mx-auto px-4 my-6 relative">
-      <h2 className="text-2xl font-semibold py-4">{heading}</h2>
+       if(checkPassword){
+        const tokenData = {
+            _id : user._id,
+            email : user.email,
+        }
+        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 8 });
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-        {loading
-          ? loadingList.map((product, index) => (
-              <div
-                key={index}
-                className="bg-white rounded-sm shadow"
-                style={{ marginBottom: "10px" }}
-              >
-                <div className="bg-slate-200 h-48 animate-pulse"></div>
-                <div className="p-4 grid gap-3">
-                  <h2 className="font-medium text-lg text-ellipsis line-clamp-1 text-black p-1 py-2 animate-pulse rounded-full bg-slate-200"></h2>
-                  <p className="capitalize text-slate-500 p-1 animate-pulse rounded-full bg-slate-200 py-2"></p>
-                  <div className="flex gap-3">
-                    <p className="text-red-600 font-medium p-1 animate-pulse rounded-full bg-slate-200 py-2"></p>
-                    <p className="text-slate-500 line-through p-1 animate-pulse rounded-full bg-slate-200 py-2"></p>
-                  </div>
-                  <button className="text-sm text-white px-3 rounded-full bg-slate-200 py-2 animate-pulse"></button>
-                </div>
-              </div>
-            ))
-          : data.map((product, index) => (
-              <Link
-                key={index}
-                to={"/product/" + product?._id}
-                className="bg-white rounded-sm shadow"
-                onClick={scrollTop}
-                style={{ marginBottom: "10px" }}
-              >
-                <div className="bg-slate-200 h-48">
-                  <img
-                    src={product.productImage[0]}
-                    alt={product.productName}
-                    className="object-cover w-full h-full hover:scale-110 transition-all"
-                  />
-                </div>
-                <div className="p-4 grid gap-3">
-                  <h2 className="font-medium text-lg text-ellipsis line-clamp-1 text-black">
-                    {product?.productName}
-                  </h2>
-                  <p className="capitalize text-slate-500">
-                    {product?.category}
-                  </p>
-                  <div className="flex gap-3">
-                    <p className="text-fuchsia-600 font-medium">
-                      {displayINRCurrency(product?.price)}
-                    </p>
-                  </div>
-                </div>
-              </Link>
-            ))}
-      </div>
-    </div>
-  );
-};
+        const tokenOption = {
+            httpOnly : true,
+            secure : true
+        }
 
-export default CategroyWiseProductDisplay;
+        res.cookie("token",token,tokenOption).status(200).json({
+            message : "Login successfully",
+            data : token,
+            success : true,
+            error : false
+        })
+
+       }else{
+         throw new Error("Please check Password")
+       }
+
+
+
+
+
+
+
+    }catch(err){
+        res.json({
+            message : err.message || err  ,
+            error : true,
+            success : false,
+        })
+    }
+
+}
+
+module.exports = userSignInController
