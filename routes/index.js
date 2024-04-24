529e5d29c34ae6ca9bfa06f6b97e9c940ecde712
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-24 09:22:31 +0500

    routes done

--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,7 @@ const updateAddToCartProduct = require('../controller/user/updateAddToCartProduc
 const deleteAddToCartProduct = require('../controller/user/deleteAddToCartProduct')
 const searchProduct = require('../controller/product/searchProduct')
 const filterProductController = require('../controller/product/filterProduct')
+const deleteProductController = require('../controller/product/deleteProduct');
 
 
 
@@ -43,6 +44,7 @@ router.post("/category-product",getCategoryWiseProduct)
 router.post("/product-details",getProductDetails)
 router.get("/search",searchProduct)
 router.post("/filter-product",filterProductController)
+router.post("/delete-product", authToken, deleteProductController);
 
 //user add to cart
 router.post("/addtocart",authToken,addToCartController)
