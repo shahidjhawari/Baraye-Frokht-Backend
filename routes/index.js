60e6a064efccca789b50b517588d3f220a8d2b7a
Mahar Shahid Iqbal Jhawari <shahidiqbaljhawari@gmail.com>
2024-04-24 09:15:56 +0500

    back to before prdct dlt done

--- routes/index.js
+++ routes/index.js
@@ -1,53 +1,60 @@
-const express = require('express');
-const router = express.Router();
-const authToken = require('../middleware/authToken');
-const deleteProductController = require('../controller/product/deleteProduct');
+const express = require('express')
 
-// Import controllers for user functionalities
-const userSignUpController = require("../controller/user/userSignUp");
-const userSignInController = require('../controller/user/userSignIn');
-const userDetailsController = require('../controller/user/userDetails');
-const userLogout = require('../controller/user/userLogout');
-const allUsers = require('../controller/user/allUsers');
-const updateUser = require('../controller/user/updateUser');
-const addToCartController = require('../controller/user/addToCartController');
-const countAddToCartProduct = require('../controller/user/countAddToCartProduct');
-const addToCartViewProduct = require('../controller/user/addToCartViewProduct');
-const updateAddToCartProduct = require('../controller/user/updateAddToCartProduct');
-const deleteAddToCartProduct = require('../controller/user/deleteAddToCartProduct');
+const router = express.Router()
 
-// Import controllers for product functionalities
-const UploadProductController = require('../controller/product/uploadProduct');
-const getProductController = require('../controller/product/getProduct');
-const updateProductController = require('../controller/product/updateProduct');
-const getCategoryProduct = require('../controller/product/getCategoryProductOne');
-const getCategoryWiseProduct = require('../controller/product/getCategoryWiseProduct');
-const getProductDetails = require('../controller/product/getProductDetails');
-const searchProduct = require('../controller/product/searchProduct');
-const filterProductController = require('../controller/product/filterProduct');
+const userSignUpController = require("../controller/user/userSignUp")
+const userSignInController = require('../controller/user/userSignIn')
+const userDetailsController = require('../controller/user/userDetails')
+const authToken = require('../middleware/authToken')
+const userLogout = require('../controller/user/userLogout')
+const allUsers = require('../controller/user/allUsers')
+const updateUser = require('../controller/user/updateUser')
+const UploadProductController = require('../controller/product/uploadProduct')
+const getProductController = require('../controller/product/getProduct')
+const updateProductController = require('../controller/product/updateProduct')
+const getCategoryProduct = require('../controller/product/getCategoryProductOne')
+const getCategoryWiseProduct = require('../controller/product/getCategoryWiseProduct')
+const getProductDetails = require('../controller/product/getProductDetails')
+const addToCartController = require('../controller/user/addToCartController')
+const countAddToCartProduct = require('../controller/user/countAddToCartProduct')
+const addToCartViewProduct  = require('../controller/user/addToCartViewProduct')
+const updateAddToCartProduct = require('../controller/user/updateAddToCartProduct')
+const deleteAddToCartProduct = require('../controller/user/deleteAddToCartProduct')
+const searchProduct = require('../controller/product/searchProduct')
+const filterProductController = require('../controller/product/filterProduct')
 
-// Routes for user functionalities
-router.post("/signup", userSignUpController);
-router.post("/signin", userSignInController);
-router.get("/user-details", authToken, userDetailsController);
-router.get("/userLogout", userLogout);
-router.get("/all-user", authToken, allUsers);
-router.post("/update-user", authToken, updateUser);
-router.post("/addtocart", authToken, addToCartController);
-router.get("/countAddToCartProduct", authToken, countAddToCartProduct);
-router.get("/view-card-product", authToken, addToCartViewProduct);
-router.post("/update-cart-product", authToken, updateAddToCartProduct);
-router.post("/delete-cart-product", authToken, deleteAddToCartProduct);
 
-// Routes for product functionalities
-router.post("/upload-product", authToken, UploadProductController);
-router.get("/get-product", getProductController);
-router.post("/update-product", authToken, updateProductController);
-router.post("/delete-product", authToken, deleteProductController); // Route for deleting product
-router.get("/get-categoryProduct", getCategoryProduct);
-router.post("/category-product", getCategoryWiseProduct);
-router.post("/product-details", getProductDetails);
-router.get("/search", searchProduct);
-router.post("/filter-product", filterProductController);
 
-module.exports = router;
+router.post("/signup",userSignUpController)
+router.post("/signin",userSignInController)
+router.get("/user-details",authToken,userDetailsController)
+router.get("/userLogout",userLogout)
+
+//admin panel 
+router.get("/all-user",authToken,allUsers)
+router.post("/update-user",authToken,updateUser)
+
+//product
+router.post("/upload-product",authToken,UploadProductController)
+router.get("/get-product",getProductController)
+router.post("/update-product",authToken,updateProductController)
+router.get("/get-categoryProduct",getCategoryProduct)
+router.post("/category-product",getCategoryWiseProduct)
+router.post("/product-details",getProductDetails)
+router.get("/search",searchProduct)
+router.post("/filter-product",filterProductController)
+
+//user add to cart
+router.post("/addtocart",authToken,addToCartController)
+router.get("/countAddToCartProduct",authToken,countAddToCartProduct)
+router.get("/view-card-product",authToken,addToCartViewProduct)
+router.post("/update-cart-product",authToken,updateAddToCartProduct)
+router.post("/delete-cart-product",authToken,deleteAddToCartProduct)
+
+
+
+
+
+
+
+module.exports = router
